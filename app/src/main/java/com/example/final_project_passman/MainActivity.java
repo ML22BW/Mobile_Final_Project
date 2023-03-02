8e1b61968f484adaf7aa77c3abed2f5d97e1ba41
wflynn82 <wflynn20@bw.edu>
2023-03-02 14:01:47 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/final_project_passman/MainActivity.java
+++ app/src/main/java/com/example/final_project_passman/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
- // HI Im Max (test)
+ // HI Im Max (test) Wyatt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
